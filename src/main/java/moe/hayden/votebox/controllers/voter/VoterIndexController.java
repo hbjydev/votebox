1a71b8337cea0823b6b19ac1bcb2ef8fed4a5ca1
Hayden Young <hi@hbjy.dev>
2022-01-21 11:04:13 +0000

    feat: implement 'Continue' button logic for voter start page

--- src/main/java/moe/hayden/votebox/controllers/voter/VoterIndexController.java
+++ src/main/java/moe/hayden/votebox/controllers/voter/VoterIndexController.java
@@ -1,4 +1,14 @@
 package moe.hayden.votebox.controllers.voter;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class VoterIndexController {
+    @FXML
+    private TextField registrationField;
+
+    @FXML
+    protected void onSubmitButtonClick() {
+        System.out.println("Voter registration: " + registrationField.getText());
+    }
 }
